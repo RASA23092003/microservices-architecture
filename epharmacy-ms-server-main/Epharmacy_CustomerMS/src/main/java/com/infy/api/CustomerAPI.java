80b84275ca20577f6528d07c8c213065eaea48bf
RASA <rasarasa23.09.2003@gmail.com>
2026-04-06 11:13:24 +0530

    completed customerMS

--- epharmacy-ms-server-main/Epharmacy_CustomerMS/src/main/java/com/infy/api/CustomerAPI.java
+++ epharmacy-ms-server-main/Epharmacy_CustomerMS/src/main/java/com/infy/api/CustomerAPI.java
@@ -1,5 +1,6 @@
 package com.infy.api;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -7,10 +8,13 @@ import javax.validation.Valid;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +24,9 @@ import com.infy.dto.CustomerAddressDTO;
 import com.infy.dto.CustomerDTO;
 import com.infy.exception.EPharmacyException;
 import com.infy.service.CustomerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin
 @RestController
@@ -29,6 +36,8 @@ public class CustomerAPI {
 	@Autowired
 	private CustomerService customerService;
 
+	@Autowired 
+	private Environment environment;
 	static Log logger = LogFactory.getLog(CustomerAPI.class);
 
 	@PostMapping(value = "customer/login")
@@ -41,51 +50,52 @@ public class CustomerAPI {
 	@PostMapping("/customer/register")
 	public ResponseEntity<String> registerCustomer(@RequestBody @Valid CustomerDTO customerDTO) throws Exception {
 		String successMessage=customerService.registerNewCustomer(customerDTO);
+		return new ResponseEntity<>(successMessage,HttpStatus.CREATED);
+	}
+
+	@GetMapping("/customer/{customerId}")
+	public ResponseEntity<CustomerDTO> viewCustomer(@RequestParam Integer customerId) throws EPharmacyException {
+		CustomerDTO customer=customerService.viewCustomer(customerId);
+		return new ResponseEntity<>(customer,HttpStatus.OK);
+	}
+	
+	@GetMapping(" /customer/view-addresses/{customerId}")
+	public ResponseEntity<List<CustomerAddressDTO>> viewAllAddress(@RequestParam Integer customerId)
+			throws EPharmacyException {
+		List<CustomerAddressDTO> addresslist=customerService.viewAllAddress(customerId);
+		return new ResponseEntity<>(addresslist,HttpStatus.OK);
+	}
+
+	@PostMapping("/customer/add-address/{customerId}")
+	public ResponseEntity<String> addAddress(@RequestBody @Valid CustomerAddressDTO caDTO,@RequestParam Integer customerId) throws EPharmacyException {
+		customerService.addCustomerAddress(caDTO, customerId);
+		String successMessage=environment.getProperty("CustomerAPI.ADDRESS_ADDED_SUCCESS");
+		return new ResponseEntity<>(successMessage,HttpStatus.CREATED);
+	}
+
+	@PutMapping("/customer/update-profile")
+	public ResponseEntity<String> updateCustomer(@RequestBody CustomerDTO cdto) throws EPharmacyException {
+		String successMessage=customerService.updateCustomerDetails(cdto);
 		return new ResponseEntity<>(successMessage,HttpStatus.OK);
 	}
 
-	
-	public ResponseEntity<CustomerDTO> viewCustomer(Integer customerId) throws EPharmacyException {
-		//write your logic here
-		return null;
+	@DeleteMapping("/customer/delete-address/{customerId}")
+	public ResponseEntity<String> deleteAddress(@RequestParam Integer addressId) throws EPharmacyException {
+		customerService.deleteAddress(addressId);
+		String successMsg=environment.getProperty("CustomerAPI.DELETE_CUSTOMER_ADDRESS_SUCCESS");
+		return new ResponseEntity<>(successMsg,HttpStatus.OK);
 	}
-	
-
-	public ResponseEntity<List<CustomerAddressDTO>> viewAllAddress(Integer customerId)
-			throws EPharmacyException {
-		//write your logic here
-		return null;
-	}
-
-	
-	public ResponseEntity<String> addAddress(CustomerAddressDTO caDTO,Integer customerId) throws EPharmacyException {
-		//write your logic here
-		return null;
-	}
-
-
-	public ResponseEntity<String> updateCustomer(CustomerDTO cdto) throws EPharmacyException {
-
-		//write your logic here
-		return null;
-	}
-
-	
-	public ResponseEntity<String> deleteAddress(Integer addressId) throws EPharmacyException {
-		//write your logic here
-		return null;
-	}
-	
-	public ResponseEntity<String> changePassword(ChangePasswordDTO cpDTO) throws Exception {
-		//write your logic here
-		return null;
+	@PutMapping("/customer/change-password")
+	public ResponseEntity<String> changePassword(@RequestBody ChangePasswordDTO cpDTO) throws Exception {
+		customerService.changePassword(cpDTO);
+		String successMsg=environment.getProperty("CustomerAPI.CHANGE_PASSWORD_SUCCESS");
+		return new ResponseEntity<>(successMsg,HttpStatus.OK);
 
 	}
-	
-
-	public ResponseEntity<String> upgradeCustomer(CustomerDTO custDTO) throws Exception {
-	
-		//write your logic here
-		return null;
+	@PutMapping("/customer/upgrade")
+	public ResponseEntity<String> upgradeCustomer(@RequestBody CustomerDTO custDTO) throws Exception {
+		LocalDate expiaryDate=customerService.upgradeCustomerToPrime(custDTO);
+		String successMsg=environment.getProperty("CustomerAPI.UPGRADE_CUSTOMER_SUCCESS");
+		return new ResponseEntity<>(successMsg,HttpStatus.OK);
 	}
 }
--- epharmacy-ms-server-main/Epharmacy_CustomerMS/src/main/java/com/infy/service/CustomerServiceImpl.java
+++ epharmacy-ms-server-main/Epharmacy_CustomerMS/src/main/java/com/infy/service/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -16,8 +17,13 @@ import com.infy.dto.CustomerAddressDTO;
 import com.infy.dto.CustomerDTO;
 import com.infy.entity.Customer;
 import com.infy.entity.CustomerAddress;
+import com.infy.entity.PasswordHistory;
+import com.infy.entity.PrimePlans;
 import com.infy.exception.EPharmacyException;
+import com.infy.repository.CustomerAddressRepository;
 import com.infy.repository.CustomerRepository;
+import com.infy.repository.PasswordHistoryRepository;
+import com.infy.repository.PrimePlansRepository;
 import com.infy.utility.HashingUtility;
 
 @Service(value = "customerService")
@@ -30,6 +36,12 @@ public class CustomerServiceImpl implements CustomerService {
 	private ObjectMapper objectMapper;
 	@Autowired
 	private Environment environment;
+	@Autowired
+	private CustomerAddressRepository customerAddressRepository;
+	@Autowired
+	private PasswordHistoryRepository passwordHistoryRepo;
+	@Autowired
+	private PrimePlansRepository planRepo;
 
 	
 	@Override
@@ -87,11 +99,12 @@ public class CustomerServiceImpl implements CustomerService {
 		Customer customer=customerRepository.findByCustomerEmailId(customerDTO.getCustomerEmailId());
 		if(customer!=null) throw new EPharmacyException("CustomerService.CUSTOMER_ALREADY_EXISTS");
 		if((LocalDate.now().getDayOfYear()-customerDTO.getDateOfBirth().getDayOfYear())<18){
-			throw new EPharmacyException("CustomerService.CUSTOMER_AGE_INVALID");
+			throw new EPharmacyException("CustomerService.INVALID_DATE");
 		}
 		Customer customerEntity=objectMapper.convertValue(customerDTO, Customer.class);
 		String hashedPassword = HashingUtility.getHashValue(customer.getPassword());
 		customerEntity.setPassword(hashedPassword);
+		customerEntity.getPlan().setPlanId(0);
 		Integer customerId=customerRepository.save(customerEntity).getCustomerId();
 		String successMessage=environment.getProperty("CustomerAPI.CUSTOMER_REGISTRATION_SUCCESS1"+"CustomerAPI.CUSTOMER_REGISTRATION_SUCCESS2"+" "+customerId);
 		return successMessage;
@@ -99,41 +112,88 @@ public class CustomerServiceImpl implements CustomerService {
 
 	@Override
 	public void addCustomerAddress(CustomerAddressDTO addressDTO, Integer customerId) throws EPharmacyException {
-		
+		Customer customer=customerRepository.findById(customerId).orElseThrow(()->new EPharmacyException("CustomerService.NO_CUSTOMER_FOUND"));
+		CustomerAddress address=objectMapper.convertValue(addressDTO, CustomerAddress.class);
+		customer.getAddressList().add(address);
+		return;
 	}
 
 	@Override
 	public CustomerDTO viewCustomer(Integer CustomerId) throws EPharmacyException {
-		//write your logic here
-		return null;
+		Customer customer=customerRepository.findById(CustomerId).orElseThrow(()->new EPharmacyException("CustomerService.NO_CUSTOMER_FOUND"));
+		List<CustomerAddressDTO> customerDTOList=new ArrayList<>();
+		for(CustomerAddress address:customer.getAddressList()){
+			customerDTOList.add(objectMapper.convertValue(address,CustomerAddressDTO.class));
+		}
+		CustomerDTO customerDto=objectMapper.convertValue(customer, CustomerDTO.class);
+		customerDto.setAddressList(customerDTOList);
+		return customerDto;
 	}
 
 	@Override
 	public void deleteAddress(Integer addressId) throws EPharmacyException {
-		//write your logic here
-
+		CustomerAddress address=customerAddressRepository.findById(addressId).orElseThrow(()->new EPharmacyException("CustomerService.NO_ADDRESS_FOUND"));
+		customerAddressRepository.delete(address);
+		return;
 	}
 
 	@Override
 	public List<CustomerAddressDTO> viewAllAddress(Integer customerId) throws EPharmacyException {
-		//write your logic here
-		return null;
+		Customer customer=customerRepository.findById(customerId).orElseThrow(()->new EPharmacyException("CustomerService.NO_CUSTOMER_FOUND"));
+		List<CustomerAddressDTO> addressDTO=new ArrayList<>();
+		for(CustomerAddress address:customer.getAddressList()){
+			addressDTO.add(objectMapper.convertValue(address, CustomerAddressDTO.class));
+		}
+		return addressDTO;
 	}
 
 	@Override
 	public String updateCustomerDetails(CustomerDTO dto) throws EPharmacyException {
-		//write your logic here
-		return null;
+		Customer customer=customerRepository.findById(dto.getCustomerId()).orElseThrow(()->new EPharmacyException("CustomerService.NO_CUSTOMER_FOUND"));
+		customer.setCustomerName(dto.getCustomerName());
+		customer.setCustomerEmailId(dto.getCustomerEmailId());
+		customer.setContactNumber(dto.getContactNumber());
+		String successMsg=environment.getProperty("CustomerAPI.UPGRADE_CUSTOMER_SUCCESS");
+		return successMsg;
 	}
 
 	@Override
-	public void changePassword(ChangePasswordDTO changePasswordDTO) throws EPharmacyException{
-		//write your logic here
+	public void changePassword(ChangePasswordDTO changePasswordDTO) throws EPharmacyException, NoSuchAlgorithmException{
+		Customer customer=customerRepository.findById(changePasswordDTO.getCustomerId()).orElseThrow(()->new EPharmacyException("CustomerService.NO_CUSTOMER_FOUND"));
+		String hashPassword=HashingUtility.getHashValue(changePasswordDTO.getOldPassword());
+		if(hashPassword!=customer.getPassword()){
+			throw new EPharmacyException("CustomerService.WRONG_PASSWORD");
+		}
+		if(!changePasswordDTO.getNewPassword().equals(changePasswordDTO.getConfirmPassword())){
+			throw new EPharmacyException("CustomerService.PASSWORDS_DO_NOT_MATCH");
+		}
+		String newhashPassword=HashingUtility.getHashValue(changePasswordDTO.getNewPassword());
+
+		List<PasswordHistory> history=passwordHistoryRepo.findByCustomer(changePasswordDTO.getCustomerId());
+		for(int i=history.size()-1;i>=history.size()-4;i--){
+			if(newhashPassword.equals(history.get(i).getPassword())){
+				throw new EPharmacyException("CustomerService.PASSWORD_FOUND_IN_HISTORY");
+			}
+		}
+		customer.setPassword(newhashPassword);
+		PasswordHistory password=objectMapper.convertValue(changePasswordDTO, PasswordHistory.class);
+		passwordHistoryRepo.save(password);
+		return;
 	}
 
 	@Override
 	public LocalDate upgradeCustomerToPrime(CustomerDTO customerDTO) throws EPharmacyException {
-		//write your logic here
-		return null;
+		int planId=customerDTO.getPlan().getPlanId();
+		Customer customer=customerRepository.findById(customerDTO.getCustomerId()).orElseThrow(()->new EPharmacyException("CustomerService.NO_CUSTOMER_FOUND"));
+		if(planId==0) throw new EPharmacyException("CustomerService.NO_PLAN_SELECTED");
+		if(customer.getPlan().getPlanId()<=0 && customer.getPlan().getPlanId()<4) throw new EPharmacyException("CustomerService.ALREADY_SUBSCRIBED");
+		PrimePlans plans=planRepo.findById(customerDTO.getPlan().getPlanId()).orElseThrow(()->new EPharmacyException("CustomerService.NO_PLAN_FOUND"));
+		customer.getPlan().setPlanId(planId);
+		LocalDate expiaryDate = null;
+		if(planId==1) expiaryDate.plusMonths(1);
+		else if(planId==2) expiaryDate.plusMonths(4);
+		else if(planId==3) expiaryDate.plusYears(1);
+		customer.setPlanExpiryDate(expiaryDate);
+		return expiaryDate;
 	}
 }
