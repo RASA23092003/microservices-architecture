f18c6e719181b5a0690bdaaf7fb0d01e180db4e8
RASA <rasarasa23.09.2003@gmail.com>
2026-05-03 20:50:00 +0530

    enabled kafka

--- epharmacy-ms-server-main/Epharmacy_MessageMS/src/main/java/com/infy/message/MessageApplication.java
+++ epharmacy-ms-server-main/Epharmacy_MessageMS/src/main/java/com/infy/message/MessageApplication.java
@@ -3,7 +3,9 @@ package com.infy.message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class MessageApplication {
 
